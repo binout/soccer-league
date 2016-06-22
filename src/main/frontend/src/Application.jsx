c987bec906ee968bee544021ba9cca2f1c637c8f
Benoit Prioux <benoit.prioux@gmail.com>
2016-06-22 22:25:56 +0200

    use react bootstrap

--- src/main/frontend/src/Application.jsx
+++ src/main/frontend/src/Application.jsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import $ from 'jquery';
 
+import { Navbar,Nav,NavItem } from 'react-bootstrap';
+
+
 require('!style!css!less!./Application.less');
 
 export class Application extends React.Component {
@@ -19,8 +22,22 @@ export class Application extends React.Component {
     }
     render() {
         return (
-            <div className="app-hello-message">
-                Hello {this.state.name} #{this.state.counter} at {this.state.timestamp}
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Planning Equipe Soccer 5</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Season</NavItem>
+                        <NavItem eventKey={2} href="#">Agenda</NavItem>
+                        <NavItem eventKey={3} href="#">Players</NavItem>
+                    </Nav>
+                </Navbar>
+                <div className="app-hello-message">
+                    Hello {this.state.name} #{this.state.counter} at {this.state.timestamp}
+                </div>
             </div>
         );
     }
